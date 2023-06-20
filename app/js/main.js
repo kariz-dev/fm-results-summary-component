89373c2278c54259a559555a338b0d52c0232de9
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2023-06-20 16:57:17 +0700

    feat(script): create new element with fething data from json

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,51 @@
+// Fetch data
 fetch('../../data.json')
   .then(response => response.json())
-  .then(data => {
-    // Handle the retrieved JSON data
-    // For example, you can pass it to a rendering function
-    renderData(data);
-    console.log(data);
-  })
+  .then(renderData)
   .catch(error => {
     console.error('Error:', error);
   });
 
-  function renderData(data) {
-    const container = document.getElementById('data');
-    
-    // Clear the container
-    container.innerHTML = '';
-    
-    // Render each item in the data
-    data.forEach(item => {
-      const listItem = document.createElement('li');
-      listItem.textContent = item.category;
-      container.appendChild(listItem);
-    });
-  }
+// Function render with new element
+function renderData(data) {
+  const parentElement = document.getElementById('summary_list');
+
+  data.forEach(item => {
+    const { icon, category, score } = item;
+
+    const liElement = document.createElement('li');
+    liElement.classList.add('summary_list_item', category.toLowerCase());
+
+    const leftDiv = document.createElement('div');
+    leftDiv.classList.add('float-left');
+
+    const imgElement = document.createElement('img');
+    imgElement.classList.add('icon');
+    imgElement.src = icon;
+    imgElement.alt = category;
+    leftDiv.appendChild(imgElement);
+
+    const pElement = document.createElement('p');
+    pElement.classList.add('name');
+    pElement.textContent = category;
+    leftDiv.appendChild(pElement);
+
+    const rightDiv = document.createElement('div');
+    rightDiv.classList.add('float-right');
+
+    const span1 = document.createElement('span');
+    span1.classList.add('point');
+    span1.textContent = score;
+    rightDiv.appendChild(span1);
+
+    const span2 = document.createElement('span');
+    span2.classList.add('pointof');
+    span2.textContent = ' / 100';
+    rightDiv.appendChild(span2);
+
+    liElement.appendChild(leftDiv);
+    liElement.appendChild(rightDiv);
+
+    parentElement.appendChild(liElement);
+  });
+}
